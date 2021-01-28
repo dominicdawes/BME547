f6bbb97bfdb249489012ba1dc00c68eea936fc86
Dominic Dawes <dominic.d.dawes@gmail.com>
2021-01-28 16:50:49 -0500

    added hdl.py on the hdl branch

---
+++ HDL.py
@@ -0,0 +1,14 @@
+#HDL in the HDL Branch
+
+def check_HDL(HDL):
+    if >= 60:
+    print("normal")
+    
+    elif 40 <= HDL < 60:
+    print("borederline low")
+    
+    else:
+    print("low")
+    return HDL
+
+interface()
